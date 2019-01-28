c995e1e62ab4c47f6d526385bcc64dd1f3653724
madyson <766799316@qq.com>
2019-01-28 15:17:56 +0800

    test

---
+++ iOS51rcProject/iOS51rcProject/Company/Search/View/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  iOS51rcProject
+//
+//  Created by Lucifer on 2019/1/28.
+//  Copyright © 2019年 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ iOS51rcProject/iOS51rcProject/Company/Search/View/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  iOS51rcProject
+//
+//  Created by Lucifer on 2019/1/28.
+//  Copyright © 2019年 Jerry. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
